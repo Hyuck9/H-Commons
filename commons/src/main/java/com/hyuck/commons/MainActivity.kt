8cb38f88ba287a7145a87f8aea8569cdb2e610c2
Hyuck9 <lhg1304@gmail.com>
2020-11-17 18:03:26 +0900

    아이콘 색상 변경, MainActivity 생성 (컬러 프리뷰 보려고)

---
+++ commons/src/main/java/com/hyuck/commons/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hyuck.commons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
